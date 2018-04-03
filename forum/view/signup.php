5afaa218bc9f9bb4e86f5beb423ea5cb51c2ef24
freezecat <freezecat.hu327@gmail.com>
2018-04-04 00:54:53 +0200

    ok

--- forum/view/signup.php
+++ forum/view/signup.php
@@ -1,31 +1,33 @@
 <span class="signup_login_title">inscription</span>
 <div id="signup_box">
-  <div>
+    <div>
   
-  <form action="index.php?page=signup" method="post"/>
+        <form action="index.php?page=signup" method="post">
     
-	<div class="margin-input">
-	<label>pseudo</label><br/>
-	<input type="text" name="pseudo" placeholder=""/>
-	</div>
+			<div class="margin-input">
+			    <label>pseudo</label><br/>
+			    <input type="text" name="pseudo" placeholder=""/>
+			</div>
 	
-	<div class="margin-input">
-    <label>email</label><br/>
-	<input type="text" name="email" placeholder=""/>
-	</div>
+			<div class="margin-input">
+			    <label>email</label><br/>
+			    <input type="text" name="email" placeholder=""/>
+			</div>
 	
-	<div class="margin-input">
-    <label>mot de passe( 8 caratères composés de majuscules , minuscules et symboles !-_.)</label><br/>
-	<input type="password" name="password" placeholder=""/>
-	</div>
+			<div class="margin-input">
+			    <label>mot de passe( 8 caratères composés de majuscules , minuscules et symboles !-_.)</label><br/>
+			    <input type="password" name="password" placeholder=""/>
+			</div>
+			
+			<div class="margin-input">
+			    <label>confirmation de mot de passe</label><br/>
+			    <input type="password" name="passwordbis" placeholder=""/>
+			</div>
+			
+			<div class="g-recaptcha" data-sitekey="YOUR_KEY"></div>
+			
+			<button type="submit" class="button button_blue">signup</button>
 	
-	<div class="margin-input">
-	 <label>confirmation de mot de passe</label><br/>
-	<input type="password" name="passwordbis" placeholder=""/>
-	</div>
-	
-	<div class="g-recaptcha" data-sitekey="6LcePz8UAAAAALeUA_5uwE0EobAKNccb-ax4vx0z"></div>
-	<button type="submit" class="button button_blue">signup</button>
-  </form>
-   </div>
+        </form>
+    </div>
 </div>
